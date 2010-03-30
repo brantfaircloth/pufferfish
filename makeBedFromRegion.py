9a806b7ca8b9b64739e82c34eecdd4ac24f4ef19
Brant Faircloth <brant.faircloth@gmail.com>
2010-03-30 09:16:42 -0700

    Adding `simple` and `advanced` methods for bed creation

--- makeBedFromRegion.py
+++ makeBedFromRegion.py
@@ -37,6 +37,8 @@ metavar='FILE')
     p.add_option('--out', '-o', dest = 'out', action='store', \
 type='string', default = None, help='The path to the output file.', \
 metavar='FILE')
+    p.add_option('--advanced', '-a', action='store_true', dest='advanced', \
+default=False, help='Produce an advanced bed file')
 
     (options,arg) = p.parse_args()
     if not options.conf:
@@ -47,6 +49,32 @@ metavar='FILE')
         p.print_help()
         sys.exit(2)
     return options, arg
+
+
+def simple(outp, data):
+    for record in data:
+        #pdb.set_trace()
+        outp.write('%s %s %s region.id_%s\n' % (record[3], record[4], record[5], record[0]))
+
+def advanced(cur, outp, gene, mrna):
+    # get gene specific data
+    query = ('''SELECT starts_exon, chromo, start, end from exons where mrna_id = %s order by start''' % (mrna))
+    cur.execute(query)
+    data = cur.fetchall()
+    blockCount = len(data)
+    chromStart = data[0][2]
+    chromEnd = data[-1][3]
+    blockSizes, blockStarts = [], []
+    for each in data:
+        exon, chrom, s, e = each
+        bsz = e - s
+        rst = s - chromStart
+        blockSizes.append(str(bsz))
+        blockStarts.append(str(rst))
+    #pdb.set_trace()
+    blockSizes = ','.join(blockSizes)
+    blockStarts = ','.join(blockStarts)
+    outp.write('%s %s %s gene_%s_mrna_%s 1000 + %s %s 0 %s %s, %s\n' % (chrom, chromStart, chromEnd, gene, mrna, chromStart, chromEnd, blockCount, blockSizes, blockStarts))
 
 def main():
     conf = ConfigParser.ConfigParser()
@@ -59,12 +87,16 @@ def main():
             )
     cur = conn.cursor()
     outp = open(options.out,'w')
-    outp.write('''track name=geneRegions description="Fugu Exons" useScore=0\n''')
-    cur.execute('''SELECT * FROM region''')
-    data = cur.fetchall()
-    for record in data:
-        #pdb.set_trace()
-        outp.write('chrUn %s %s region.id_%s\n' % (record[1], record[2], record[0]))
+    outp.write('''track name=geneRegions description="Tetraodon Exons" useScore=0\n''')
+    if not options.advanced:
+        cur.execute('''SELECT * FROM exons''')
+        data = cur.fetchall()
+        simple(outp, data)
+    if options.advanced:
+        # get list of "genes"
+        cur.execute('''SELECT id, mrna_id from genes''')
+        for gene, mrna in cur.fetchall():
+            advanced(cur, outp, gene, mrna)
     outp.close()
     cur.close()
     conn.close()
